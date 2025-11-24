fb97765ad0c0050b1469a43c194edfe734a7a1cb
153260 <114780061+153260@users.noreply.github.com>
2025-11-24 21:11:53 +0330

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,23 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// You can get this from the Firebase Console -> Project Settings -> General -> Your apps
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_AUTH_DOMAIN_HERE",
-    projectId: "YOUR_PROJECT_ID_HERE",
-    storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-    appId: "YOUR_APP_ID_HERE"
+    apiKey: "AIzaSyDIxPxOPx65fta0iuFuLtyASVMX8MniN_8",
+    authDomain: "frdrick-17f7d.firebaseapp.com",
+    projectId: "frdrick-17f7d",
+    storageBucket: "frdrick-17f7d.firebasestorage.app",
+    messagingSenderId: "910362596354",
+    appId: "1:910362596354:web:0b778efa5a71f42cd1031b",
+    measurementId: "G-BX68HN01TS"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 const googleProvider = new GoogleAuthProvider();
 
 export { auth, db, googleProvider };
